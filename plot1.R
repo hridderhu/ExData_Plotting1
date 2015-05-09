94a95e31ae77af9beebafe9b71dfc6464ffd8a32
Henk van de Ridder <henk.vanderidder@hu.nl>
2015-05-09 11:49:22 +0200

    Plot1 ready

---
+++ plot1.R
@@ -0,0 +1,23 @@
+#
+# plot1.R
+#
+
+setwd("H:\\Datascientist\\04-ExData\\CourseProject1")
+energyall <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
+energyall <- transform(energyall, Date = as.Date(Date,format="%d/%m/%Y"))
+
+# subset of the days
+energy <- subset(energyall, energyall$Date %in% c(as.Date('2007-02-01',format="%Y-%m-%d"), as.Date('2007-02-02',format="%Y-%m-%d")))
+
+# add day-name (in Dutch)
+energy <- transform(energy, Day = format(Date,format="%a"))
+
+# set output device
+png(filename = "plot1.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white", res = NA, family = "", restoreConsole = TRUE,
+    type = c("windows", "cairo", "cairo-png"))
+
+hist(energy$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+
+dev.off()
